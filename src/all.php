eaa29841e3ef6fc4aa9d529421ef4182f0f85a44
hiapples <howard.2001@yahoo.com.tw>
2024-08-18 00:57:24 +0800

    3

--- src/all.php
+++ src/all.php
@@ -56,7 +56,7 @@
         </div>
     </nav>
     <!-- content -->
-    <div class="container content  scroll2 mt-3 mb-5">
+    <div class="container  scroll2 mt-3 mb-5">
         <div class="row " id="all">
             
         </div>
